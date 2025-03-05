63dbdef33d8737f0cc7037b472e769cc446455fa
ViktorMathe <matheviktor@icloud.com>
2025-03-05 00:56:45 +0000

    DB settings

--- wheels_next_to_sea/settings.py
+++ wheels_next_to_sea/settings.py
@@ -83,15 +83,7 @@ WSGI_APPLICATION = 'wheels_next_to_sea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if 'DEVELOPMENT' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3'
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.postgresql_psycopg2',
                 'NAME': str('defaultdb'),
