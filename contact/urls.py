93e2ef66606d9bb029a6a91d801fe3ee0e644fb4
Viktor Mathe <99300061+ViktorMathe@users.noreply.github.com>
2025-09-20 23:48:01 +0100

    Fixed url

--- contact/urls.py
+++ contact/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("contact/", views.contact_page, name="contact_page"),
+    path("/", views.contact_page, name="contact_page"),
     path("admin/reply-contact/", views.admin_reply_contact, name="admin_reply_contact"),
 ]
