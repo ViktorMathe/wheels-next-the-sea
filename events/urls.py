41f1da0c59d28aff2e82a35b5d7a97308b13a894
Viktor Mathe <99300061+ViktorMathe@users.noreply.github.com>
2025-09-21 16:53:02 +0100

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('current-events/', views.current_events, name='current_events'),
     path('past-events/', views.past_events, name='past_events'),
     path('add-event/', views.manage_events, name='add_event'),
-    path('manage-events/<int:event_id>/', views.manage_events, name='manage_events_edit'), 
+    path('manage-events/<int:event_id>/', views.manage_events, name='manage_events_edit'),
+    path("events/delete/<int:event_id>/", views.delete_event, name="delete_event"),
 ]
