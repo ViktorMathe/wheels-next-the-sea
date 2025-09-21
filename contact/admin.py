56beb3d63fd2246c53147c44d7e0a5f04ca6dbe7
Viktor Mathe <99300061+ViktorMathe@users.noreply.github.com>
2025-09-21 20:02:36 +0100

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from .models import ContactNotification
 
+
 @admin.register(ContactNotification)
 class ContactNotificationAdmin(admin.ModelAdmin):
-    filter_horizontal = ('recipients',)  # nice multi-select widget
-    
+    list_display = ('id', 'get_recipients')
+
     def get_recipients(self, obj):
         return ", ".join([user.email for user in obj.recipients.all()])
     get_recipients.short_description = "Recipients"
